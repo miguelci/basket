63d4afa25c17f2dd56b9d9a83912707d9ffc7946
Miguel Heitor <miguel.heitor@castoredc.com>
2019-10-15 21:27:16 +0200

    Update app.php

--- app.php
+++ app.php
@@ -1,28 +1,34 @@
 <?php
 
+use Basket\EventBus\EventBusFactory;
+use Basket\Events\AddProductToBasket;
+use Basket\Events\CreateBasket;
+use Basket\Events\RemoveProductFromBasket;
+use Basket\Objects\Basket;
+use Basket\Objects\Product;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$eventBus = \Basket\EventBus\EventBusFactory::makeEventBus();
+$eventBus = EventBusFactory::makeEventBus();
 
-$basket     = new \Basket\Objects\Basket();
-$mouse      = new \Basket\Objects\Product('Mouse', 20);
-$monitor    = new \Basket\Objects\Product('Monitor', 40);
-$computer   = new \Basket\Objects\Product('Computer PC', 450);
-$mac        = new \Basket\Objects\Product('MacBook', 950);
-$watch      = new \Basket\Objects\Product('Watch', 250);
-$headphones = new \Basket\Objects\Product('Headphones', 150);
+$basket     = new Basket();
+$mouse      = new Product('Mouse', 20);
+$monitor    = new Product('Monitor', 40);
+$computer   = new Product('Computer PC', 450);
+$mac        = new Product('MacBook', 950);
+$watch      = new Product('Watch', 250);
+$headphones = new Product('Headphones', 150);
 
 
-$eventBus->addEvent(new \Basket\Events\CreateBasket($basket));
-$eventBus->addEvent(new \Basket\Events\AddProductToBasket($basket, $mouse));
-$eventBus->addEvent(new \Basket\Events\AddProductToBasket($basket, $monitor));
-$eventBus->addEvent(new \Basket\Events\RemoveProductFromBasket($basket, $mouse));
-$eventBus->addEvent(new \Basket\Events\AddProductToBasket($basket, $computer));
-$eventBus->addEvent(new \Basket\Events\AddProductToBasket($basket, $mac));
-$eventBus->addEvent(new \Basket\Events\AddProductToBasket($basket, $watch));
-$eventBus->addEvent(new \Basket\Events\RemoveProductFromBasket($basket, $computer));
-$eventBus->addEvent(new \Basket\Events\AddProductToBasket($basket, $headphones));
-
+$eventBus->addEvent(new CreateBasket($basket));
+$eventBus->addEvent(new AddProductToBasket($basket, $mouse));
+$eventBus->addEvent(new AddProductToBasket($basket, $monitor));
+$eventBus->addEvent(new RemoveProductFromBasket($basket, $mouse));
+$eventBus->addEvent(new AddProductToBasket($basket, $computer));
+$eventBus->addEvent(new AddProductToBasket($basket, $mac));
+$eventBus->addEvent(new AddProductToBasket($basket, $watch));
+$eventBus->addEvent(new RemoveProductFromBasket($basket, $computer));
+$eventBus->addEvent(new AddProductToBasket($basket, $headphones));
 
 //print_r($eventBus->serialize());
 
